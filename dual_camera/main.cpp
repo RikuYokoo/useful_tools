4d78c96aad44353f036d82fe50b5fa00010cc316
RikuYokoo <s19C1123JF@s.chibakoudai.jp>
2024-01-19 19:26:15 +0900

    時間計測を追加

--- dual_camera/main.cpp
+++ dual_camera/main.cpp
@@ -1,9 +1,11 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #include <vector>
+#include <chrono>
 
 int main(int argc, char* argv[])
 {
+  using namespace std::literals::chrono_literals;
   cv::VideoCapture cap0, cap1;
   cap0.open(0, cv::CAP_V4L2);
   cap1.open(1, cv::CAP_V4L2);
@@ -16,6 +18,7 @@ int main(int argc, char* argv[])
   cv::Mat frame0, frame1;
   while(true)
   {
+    auto start = std::chrono::high_resolution_clock::now();
     cap0.read(frame0);
     cap1.read(frame1);
 
@@ -33,6 +36,12 @@ int main(int argc, char* argv[])
       std::cout << "end" << std::endl;
       return 0;
     }
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    double ms_time = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000.;
+
+    std::cout << ms_time << " [ms] " << std::endl;
 
   }
 
